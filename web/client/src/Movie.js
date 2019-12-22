bc92f402d6f179a8c83ada57bb39e9f3d2d6a851
zouyonghao <yonghaoz1994@gmail.com>
2019-12-22 19:49:47 +0800

    show image of movies

--- web/client/src/Movie.js
+++ web/client/src/Movie.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Async from 'react-async'
 import { ListItem } from '@material-ui/core';
 
 const movies = require("./movies.json")
+const links = require("./links.json")
+
+const omdbAPI = "http://www.omdbapi.com/?apikey=24e65b04&i=tt"
+
+const loadData = ({imdbID}) =>
+    fetch(omdbAPI + imdbID)
+        .then(res => (res.ok ? res : Promise.reject(res)))
+        .then(res => res.json())
 
 export default function Movie({ movie }) {
 
@@ -9,8 +18,14 @@ export default function Movie({ movie }) {
     if (movieDetails) {
         return (
             <ListItem className='movie'>
-                <div className="flex-align-mid">
-                    {movieDetails.title.substr(0, 50)}
+                <div>
+                    <Async promiseFn={loadData} imdbID={links[movie]}>
+                        {({ data }) => {
+                            if (data)
+                                return (<img src={data.Poster} width="100" alt={movieDetails.title.substr(0, 50)}/>)
+                        }
+                        }
+                    </Async>
                 </div>
             </ListItem>
         )
